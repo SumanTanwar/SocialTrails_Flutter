306696f910a0cc398160925ad43247296de192e6
Barsha Roka Magar <barsharoka2626@gmail.com>
2024-10-06 18:45:01 -0400

    created new page admin change password

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:socialtrailsapp/Adminpanel/adminchangepassword.dart';
 import 'package:socialtrailsapp/Adminpanel/adminsetting.dart';
 import 'package:socialtrailsapp/Utility/SessionManager.dart';
 import 'package:socialtrailsapp/splashscreen.dart';
@@ -26,7 +27,7 @@ class MyApp extends StatelessWidget {
         primaryColor: Colors.purple,
         scaffoldBackgroundColor: Colors.white,
       ),
-      home: SessionManager().isLoggedIn() ?  const UserDashboardScreen() : AdminSettingsScreen(),
+      home: SessionManager().isLoggedIn() ?  const UserDashboardScreen() : AdminChangePasswordScreen(),
     );
   }
 }
