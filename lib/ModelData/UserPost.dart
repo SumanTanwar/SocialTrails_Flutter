43a4b7fbb42d49de5378c0f136c3a232b47010d8
krupa <krupa@KRUPA>
2024-10-07 14:26:03 -0400

    changes in model fields

--- lib/ModelData/UserPost.dart
+++ lib/ModelData/UserPost.dart
@@ -3,10 +3,10 @@ import '../Utility/Utils.dart';
 class UserPost {
   String? postId;
   String userId;
-  String captionText;
-  String? createdOn;
-  String? updatedOn;
-  bool postDeleted;
+  String captiontext;
+  String? createdon;
+  String? updatedon;
+  bool postdeleted;
   bool? flagged;
   bool? moderationStatus;
   List<Uri> imageUris;
@@ -17,21 +17,21 @@ class UserPost {
   UserPost({
     this.postId,
     required this.userId,
-    required this.captionText,
+    required this.captiontext,
     required this.imageUris,
     required this.location,
     required this.longitude,
     required this.latitude,
-  })  : createdOn = Utils.getCurrentDatetime(),
-        postDeleted = false;
+  })  : createdon = Utils.getCurrentDatetime(),
+        postdeleted = false;
 
   Map<String, dynamic> toJson() {
     return {
       'postId': postId,
       'userId': userId,
-      'captionText': captionText,
-      'createdOn': createdOn,
-      'postDeleted': postDeleted,
+      'captiontext': captiontext,
+      'createdon': createdon,
+      'postdeleted': postdeleted,
       'location': location,
       'latitude': latitude,
       'longitude': longitude
