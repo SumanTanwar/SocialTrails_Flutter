ecc8bfd4e5a4d48ea3fa3597906292b1e19d339a
Barsha Roka Magar <barsharoka2626@gmail.com>
2024-10-29 21:02:07 -0400

    report user is done

--- lib/follounfollow.dart
+++ lib/follounfollow.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:socialtrailsapp/Interface/OperationCallback.dart';
+import 'package:socialtrailsapp/ModelData/Report.dart';
+import 'package:socialtrailsapp/Utility/ReportService.dart';
 import 'package:socialtrailsapp/Utility/SessionManager.dart';
 import 'package:socialtrailsapp/Utility/UserService.dart';
 import 'package:socialtrailsapp/ModelData/Users.dart';
@@ -26,7 +28,6 @@ class _FollowUnfollowViewState extends State<FollowUnfollowView> {
   List<String> _postImages = [];
   int postsCount = 0;
 
-
   @override
   void initState() {
     super.initState();
@@ -40,8 +41,6 @@ class _FollowUnfollowViewState extends State<FollowUnfollowView> {
       isLoading = false;
     });
   }
-
-
 
   Future<void> _fetchUserPosts(String userId) async {
     await userPostService.getAllUserPost(userId, DataOperationCallback<List<UserPost>>(
@@ -66,13 +65,116 @@ class _FollowUnfollowViewState extends State<FollowUnfollowView> {
 
   void _followUser() {
     // Add your follow logic here, e.g., API call to follow the user
-    print("Followed user with ID");
+    print("Followed user with ID: ${widget.userId}");
+  }
+
+  void openReportDialog(BuildContext context, String userId) {
+    String reason = "";
+
+    showDialog(
+      context: context,
+      builder: (context) {
+        return AlertDialog(
+          contentPadding: EdgeInsets.all(16.0),
+          title: Text(
+            'Report User',
+            style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+            textAlign: TextAlign.center,
+          ),
+          content: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              Text(
+                'Why are you reporting this user?',
+                style: TextStyle(fontSize: 14, fontWeight: FontWeight.bold),
+              ),
+              SizedBox(height: 5),
+              Text(
+                'Your report is anonymous. If someone is in immediate danger, call the local emergency service.',
+                style: TextStyle(fontSize: 12),
+                textAlign: TextAlign.start,
+              ),
+              SizedBox(height: 8),
+              TextField(
+                maxLines: 5,
+                onChanged: (value) {
+                  reason = value;
+                },
+                decoration: InputDecoration(
+                  hintText: 'Describe the issue here',
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(5.0),
+                    borderSide: BorderSide(
+                      color: Colors.grey,
+                      width: 1.0,
+                    ),
+                  ),
+                  contentPadding: EdgeInsets.all(8.0),
+                ),
+              ),
+            ],
+          ),
+          actions: [
+            TextButton(
+              onPressed: () {
+                if (reason.isNotEmpty) {
+                  reportUser(userId, reason);
+                  Navigator.of(context).pop();
+                } else {
+                  ScaffoldMessenger.of(context).showSnackBar(
+                    SnackBar(content: Text('Please provide a reason for reporting.')),
+                  );
+                }
+              },
+              child: Text('Report'),
+            ),
+            TextButton(
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+              child: Text('Cancel'),
+            ),
+          ],
+        );
+      },
+    );
+  }
+
+  void reportUser(String userId, String reason) {
+    Report report = Report(
+      createdon: DateTime.now().toString(),
+      reason: reason,
+      reportedid: userId,
+      reporterid: SessionManager().getUserID()!,
+      reporttype: 'user',
+      status: 'pending',
+    );
+
+    ReportService reportService = ReportService();
+
+    reportService.addReport(
+      report,
+      OperationCallback(
+        onSuccess: () {
+          ScaffoldMessenger.of(context).showSnackBar(
+            SnackBar(content: Text('Report submitted successfully!')),
+          );
+        },
+        onFailure: (error) {
+          ScaffoldMessenger.of(context).showSnackBar(
+            SnackBar(content: Text('Something went wrong! Please try again later.')),
+          );
+        },
+      ),
+    );
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Padding(
+        appBar: AppBar(
+        title: Text('User Profile'),),
+    body: Padding(
         padding: const EdgeInsets.all(8.0),
         child: Container(
           margin: const EdgeInsets.only(top: 50),
@@ -99,15 +201,15 @@ class _FollowUnfollowViewState extends State<FollowUnfollowView> {
                     ],
                   ),
                   SizedBox(width: 15),
-                  ProfileStat(count: '0', label: 'Posts'), // You may want to fetch this data as well
+                  ProfileStat(count: postsCount.toString(), label: 'Posts'), // Updated to show actual post count
                   SizedBox(width: 15),
-                  ProfileStat(count: '0', label: 'Followers'), // You may want to fetch this data as well
+                  ProfileStat(count: '0', label: 'Followers'), // Placeholder for followers count
                   SizedBox(width: 15),
-                  ProfileStat(count: '0', label: 'Followings'), // You may want to fetch this data as well
+                  ProfileStat(count: '0', label: 'Followings'), // Placeholder for followings count
                 ],
               ),
               SizedBox(height: 5),
-              UserDetailText(label: user?.bio ?? ''),
+              UserDetailText(label: user?.bio ?? '', onReportPressed: () => openReportDialog(context, user!.userId)),
               SizedBox(height: 10),
 
               Padding(
@@ -115,13 +217,13 @@ class _FollowUnfollowViewState extends State<FollowUnfollowView> {
                 child: SizedBox(
                   width: double.infinity,
                   child: ElevatedButton(
-                    onPressed: _followUser, // Your save function here
+                    onPressed: _followUser,
                     style: ElevatedButton.styleFrom(
-                      backgroundColor: Colors.purple, // Button color
-                      padding: const EdgeInsets.symmetric(vertical: 10.0), // Vertical padding
-                      textStyle: const TextStyle(fontSize: 16), // Text style
+                      backgroundColor: Colors.purple,
+                      padding: const EdgeInsets.symmetric(vertical: 10.0),
+                      textStyle: const TextStyle(fontSize: 16),
                     ),
-                    child: Text('Follow', style: TextStyle(color: Colors.white)), // Button text
+                    child: Text('Follow', style: TextStyle(color: Colors.white)),
                   ),
                 ),
               ),
@@ -148,7 +250,6 @@ class _FollowUnfollowViewState extends State<FollowUnfollowView> {
                 ),
               )
                   : Text("No posts available"),
-
             ],
           ),
         ),
@@ -183,18 +284,30 @@ class ProfileStat extends StatelessWidget {
 
 class UserDetailText extends StatelessWidget {
   final String label;
+  final VoidCallback onReportPressed; // Callback for reporting
 
-  UserDetailText({required this.label});
+  UserDetailText({required this.label, required this.onReportPressed});
 
   @override
   Widget build(BuildContext context) {
     return Container(
       alignment: Alignment.centerLeft,
       padding: EdgeInsets.only(left: 10),
-      child: Text(
-        label,
-        style: TextStyle(fontSize: 12, color: Colors.black),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Text(
+            label,
+            style: TextStyle(fontSize: 12, color: Colors.black),
+          ),
+          IconButton(
+            icon: Icon(Icons.warning), // Icon for reporting
+            onPressed: onReportPressed,
+          ),
+        ],
       ),
     );
+
   }
 }
+
